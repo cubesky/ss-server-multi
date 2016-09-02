84b4e8a1e0ef6a862d19229bcf80f0a49a58ff0e
cubesky <max_301@live.com>
2016-09-03 02:27:25 +0800

    Full

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,8 @@ RUN mkdir /home/ss
 WORKDIR /home/ss
 RUN wget https://raw.githubusercontent.com/cubesky/ss-server-multi/master/set.sh -O set.sh
 
+EXPOSE 10010
+EXPOSE 10020
+EXPOSE 10030
+
 ENTRYPOINT ["sh","set.sh"] 
--- set.sh
+++ set.sh
@@ -1,12 +1,12 @@
 if [ $SSPORT1 ];then
     echo ss-server Port 10010
-    ss-server -s 0.0.0.0 -m aes-256-cfb -p 10010 -k $SSPORT1
+    /usr/local/bin/ss-server -s 0.0.0.0 -m aes-256-cfb -p 10010 -k $SSPORT1 &
 fi
 if [ $SSPORT2 ];then
     echo ss-server Port 10020
-    ss-server -s 0.0.0.0 -m aes-256-cfb -p 10020 -k $SSPORT2
+    /usr/local/bin/ss-server -s 0.0.0.0 -m aes-256-cfb -p 10020 -k $SSPORT2 &
 fi
 if [ $SSPORT3 ];then
     echo ss-server Port 10030
-    ss-server -s 0.0.0.0 -m aes-256-cfb -p 10030 -k $SSPORT3
+    /usr/local/bin/ss-server -s 0.0.0.0 -m aes-256-cfb -p 10030 -k $SSPORT3 &
 fi
